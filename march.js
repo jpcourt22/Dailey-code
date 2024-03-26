3ee80d49ea5d0db0f92a8b8902f784af30f9ef5b
Joshua Courtney <joshuapatrick.courtney.com>
2024-03-26 09:38:57 -0400

    3/26/2024 Updated

---
+++ march.js
@@ -0,0 +1,18 @@
+// Return the Sum of Two Numbers March/26/2024 Updated
+// Create a function that takes two numbers as arguments and returns their sum.
+function addition(num1, num2) {
+    return num1 + num2;
+}
+// Examples
+addition(3, 2);
+// ➞ 5
+
+addition(-3, -6);
+// ➞ -9
+
+addition(7, 3);
+// ➞ 10
+// Notes
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.

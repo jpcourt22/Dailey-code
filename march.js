12a5bfd05e3c146ec44e57b155afa2789d8ba0fb
Joshua Courtney <joshuapatrick.courtney.com>
2024-03-27 06:48:45 -0400

    3/27/2024

--- march.js
+++ march.js
@@ -1,17 +1,34 @@
-// Return the Sum of Two Numbers March/26/2024 Updated
-// Create a function that takes two numbers as arguments and returns their sum.
-function addition(num1, num2) {
-    return num1 + num2;
-}
+// // Return the Sum of Two Numbers March/26/2024 Updated
+// // Create a function that takes two numbers as arguments and returns their sum.
+// function addition(num1, num2) {
+//     return num1 + num2;
+// }
+// // Examples
+// addition(3, 2);
+// // ➞ 5
+
+// addition(-3, -6);
+// // ➞ -9
+
+// addition(7, 3); March/27/2024
+// // ➞ 10
+// // Notes
+// // Don't forget to return the result.
+// // If you get stuck on a challenge, find help in the Resources tab.
+// // If you're really stuck, unlock solutions in the Solutions tab.
+
+// Convert Minutes into Seconds
+// Write a function that takes an integer minutes and converts it to seconds.
+
 // Examples
-addition(3, 2);
-// ➞ 5
+convert(5);
+// ➞ 300
 
-addition(-3, -6);
-// ➞ -9
+convert(3);
+//  ➞ 180
 
-addition(7, 3);
-// ➞ 10
+convert(2);
+// ➞ 120
 // Notes
 // Don't forget to return the result.
 // If you get stuck on a challenge, find help in the Resources tab.

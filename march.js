5e12bc1c39938418c76fb8b729f167a5f8e06769
Joshua Courtney <joshuapatrick.courtney.com>
2024-04-03 07:10:05 -0400

    4/3/24

--- march.js
+++ march.js
@@ -36,21 +36,38 @@
 // If you get stuck on a challenge, find help in the Resources tab.
 // If you're really stuck, unlock solutions in the Solutions tab.
 
-// Return the Next Number from the Integer Passed March/29/2024 Updated
-// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
-function addition(num1) {
-    return num1 + 1;
-}
-// Examples
-addition(0);
-// ➞ 1
+// // Return the Next Number from the Integer Passed March/29/2024 Updated
+// // Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+// function addition(num1) {
+//     return num1 + 1;
+// }
+// // Examples
+// addition(0);
+// // ➞ 1
 
-addition(9);
-//  ➞ 10
+// addition(9);
+// //  ➞ 10
 
-addition(-3);
-// ➞ -2
-// Notes
-// Don't forget to return the result.
+// addition(-3);
+// // ➞ -2
+// // Notes
+// // Don't forget to return the result.
 // If you get stuck on a challenge, find help in the Resources tab.
 // If you're really stuck, unlock solutions in the Solutions tab.
+
+// Convert Age to Days April/3/2024
+// Create a function that takes the age in years and returns the age in days.
+
+// Examples
+calcAge(65);
+// ➞ 23725
+
+calcAge(0);
+// ➞ 0
+
+calcAge(20);
+// ➞ 7300
+// Notes
+// Use 365 days as the length of a year for this challenge.
+// Ignore leap years and days between last birthday and now.
+// Expect only positive integer inputs.
